f0a3e68febfad96379f8ecd13920f99600f550fe
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-01 05:00:39 +0500

    sign in and sign up ui completed

--- providers/AppProvider.tsx
+++ providers/AppProvider.tsx
@@ -1,9 +1,12 @@
 "use client";
 import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
-	return <NextUIProvider>{children}</NextUIProvider>;
+	const router = useRouter();
+
+	return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
 };
 
 export default AppProvider;
