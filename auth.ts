a18a122a1b64b89f5b4defbb07c9cdbc52f24b74
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-02 05:40:25 +0500

    cretdentials provider done

--- auth.ts
+++ auth.ts
@@ -1,11 +1,32 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import prisma from "./prisma/db";
+import { compare } from "bcryptjs";
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [
 		Credentials({
 			credentials: {
 				email: {},
 				password: {},
+			},
+			authorize: async ({ email, password }) => {
+				const user = await prisma.user.findFirst({
+					where: {
+						email: email as string,
+					},
+				});
+				if (!user) {
+					throw new Error("User does not exist");
+				} else {
+					const isPasswordCorrect = await compare(
+						password as string,
+						user.password!
+					);
+					if (!isPasswordCorrect) {
+						throw new Error("Password is incorrect");
+					}
+				}
+				return user;
 			},
 		}),
 	],
